657ee72abf007e7f86132376b29a3920e822bc4b
edisonleonardo <edison.leonardo@gmail.com>
2015-11-20 12:04:21 -0500

    cambios varios

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+
+	public GameObject superCookie;
+	private GameObject activeSuperCookie;
+
+	// Use this for initialization
+	void Start () {
+		activeSuperCookie = Instantiate (superCookie);
+		StartCoroutine (ScheduleCoookieDestroy ());
+	}
+	
+	IEnumerator ScheduleCoookieDestroy(){
+		yield return new  WaitForSeconds (3.0f);
+		Destroy (activeSuperCookie);
+	}
+
+}
